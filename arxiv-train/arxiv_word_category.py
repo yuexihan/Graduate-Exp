15bbe75705f03c00a2b85a4609f265b2484c6c17
leon <463691529@qq.com>
2017-11-26 21:00:08 +0800

    convert indent from tab to space

--- arxiv-train/arxiv_word_category.py
+++ arxiv-train/arxiv_word_category.py
@@ -12,27 +12,32 @@ f = open('belong_arxiv.json')
 Belong = json.load(f)
 
 inParentheses = re.compile(r'\(([^)]+)\)')
+
+
 def get_categories(s):
-	ss = inParentheses.findall(s)
-	return [Belong[s.lower()] for s in ss]
+    ss = inParentheses.findall(s)
+    return [Belong[s.lower()] for s in ss]
+
 
 def unify(s):
-	if isinstance(s, str):
-		return s.decode('utf-8')
-	else:
-		return s
+    if isinstance(s, str):
+        return s.decode('utf-8')
+    else:
+        return s
+
+
 with open('arxiv_word_category.csv', 'wb') as fout:
-	writer = csv.writer(fout)
-	writer.writerow(['paperid', 'words', 'category'])
-	with open('arxiv.csv', 'rb') as fin:
-		reader = csv.reader(fin)
-		reader.next()
-		for row in reader:
-			(paperid, title, authors, abstract, categories) = row
-			words = punctuation.split(unify(title.lower())) + punctuation.split(unify(abstract.lower()))
-			words = [word for word in words if word]
-			words = json.dumps(words)
-			categories = get_categories(categories)
-			for category in categories:
-				writer.writerow([paperid, words, category])
+    writer = csv.writer(fout)
+    writer.writerow(['paperid', 'words', 'category'])
+    with open('arxiv.csv', 'rb') as fin:
+        reader = csv.reader(fin)
+        reader.next()
+        for row in reader:
+            (paperid, title, authors, abstract, categories) = row
+            words = punctuation.split(unify(title.lower())) + punctuation.split(unify(abstract.lower()))
+            words = [word for word in words if word]
+            words = json.dumps(words)
+            categories = get_categories(categories)
+            for category in categories:
+                writer.writerow([paperid, words, category])
 
