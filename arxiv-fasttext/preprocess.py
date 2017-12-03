f0a5fb5111683ff88f527bf755452a740400addb
leon <463691529@qq.com>
2017-12-03 11:56:35 +0800

    add fasttext preprocessing

---
+++ arxiv-fasttext/preprocess.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+
+import csv
+import json
+import re
+import nltk
+from tqdm import tqdm
+
+punctuation = re.compile(ur"[\s+=`^!?\\/_.,$%^*()\[\]:;\"\'——！<>{}|，。？、~@#￥%…&（）：；‘’《》“”»〔〕]+")
+ill = re.compile(ur'[\d-]')
+
+f = open('belong_arxiv.json')
+Belong = json.load(f)
+
+inParentheses = re.compile(r'\(([^)]+)\)')
+
+
+def get_categories(s):
+    ss = inParentheses.findall(s)
+    return [Belong[s.lower()] for s in ss]
+
+
+def unify(s):
+    if isinstance(s, str):
+        return s.decode('utf-8')
+    else:
+        return s
+
+
+with open('../data/arxiv_categories_words_fasttext.txt', 'wb') as fout:
+    with open('../data/arxiv.csv', 'rb') as fin:
+        reader = csv.reader(fin)
+        reader.next()
+        for row in tqdm(reader):
+            (paperid, title, authors, abstract, categories) = row
+            text = re.sub(r'[\r\n]+', ' ', (title + abstract).decode('utf-8'))
+            words = ' '.join(nltk.word_tokenize(text))
+            categories = inParentheses.findall(categories)
+            categories = ' '.join(['__label__' + c for c in categories])
+            fout.write(categories + ' ' + words + '\n')
--- arxiv-train/TFIDF/wordCount.py
+++ arxiv-train/TFIDF/wordCount.py
@@ -14,28 +14,30 @@ Belong_extend = json.load(f)
 f = open('../allwords.json')
 Allwords = set(json.load(f))
 
+
 def unify(s):
-	if isinstance(s, str):
-		return s.decode('utf-8')
-	else:
-		return s
+    if isinstance(s, str):
+        return s.decode('utf-8')
+    else:
+        return s
 
 # vocabulary = set()
 vocabulary = {}
 
+
 with open('../arxiv.csv', 'rb') as fin:
-	reader = csv.reader(fin)
-	for row in reader:
-		(paperid, title, authors, abstract, categories) = row
-		words = punctuation.split(unify(title.strip().lower())) + punctuation.split(abstract.strip().lower())
-		words = [word for word in words if word]
-		for word in words:
-			if word and not ill.search(word):
-				# vocabulary.add(word)
-				if word in vocabulary:
-					vocabulary[word] += 1
-				else:
-					vocabulary[word] = 1
+    reader = csv.reader(fin)
+    for row in reader:
+        (paperid, title, authors, abstract, categories) = row
+        words = punctuation.split(unify(title.strip().lower())) + punctuation.split(abstract.strip().lower())
+        words = [word for word in words if word]
+        for word in words:
+            if word and not ill.search(word):
+                # vocabulary.add(word)
+                if word in vocabulary:
+                    vocabulary[word] += 1
+                else:
+                    vocabulary[word] = 1
 
 # print vocabulary
 print len(vocabulary)
