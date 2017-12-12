fb77604a5acc0c5e090a631ced575aba07be7820
leon <463691529@qq.com>
2017-12-12 10:53:31 +0800

    add fasttext visualize

---
+++ arxiv-fasttext/tf_fasttext_visualize.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import numpy as np
+import os
+from tensorflow.contrib.tensorboard.plugins import projector
+
+paper_embedding = np.array([1216315, 50], dtype=np.float32)
+with open('data/arxiv_fasttext_vector.txt') as f:
+    for i, line in enumerate(f):
+        for j, x in enumerate(line.split()):
+            x = float(x)
+            paper_embedding[i, j] = x
+
+paper_embedding = tf.Variable(paper_embedding, trainable=False, name='paper_embedding')
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+config = projector.ProjectorConfig()
+
+paper_projector = config.embeddings.add()
+paper_projector.tensor_name = paper_embedding.name
+paper_projector.metadata_path = 'data/arxiv_main_category.txt'
+
+if not os.path.exists('save'):
+    os.makedirs('save')
+summary_writer = tf.summary.FileWriter('save')
+projector.visualize_embeddings(summary_writer, config)
+
---
+++ arxiv-lda/README.md
@@ -0,0 +1,5 @@
+INFO:lda:n_documents: 1826751
+INFO:lda:vocab_size: 50000
+INFO:lda:n_words: 185060756
+INFO:lda:n_topics: 50
+INFO:lda:n_iter: 2000
