9c3cafb54e19918b43727d6d9cdf7d7fdb3fb2c2
leon <463691529@qq.com>
2017-12-17 13:24:21 +0800

    sentence2vec

--- sentence2vec/main.py
+++ sentence2vec/main.py
@@ -16,8 +16,13 @@ from word2vec import Sent2Vec, LineSentence
 logging.basicConfig(format='%(asctime)s : %(threadName)s : %(levelname)s : %(message)s', level=logging.INFO)
 logging.info("running %s" % " ".join(sys.argv))
 
+input_file = 'arxiv_words.txt'
+model = Word2Vec(LineSentence(input_file), size=100, window=5, sg=0, min_count=5, workers=8)
+model.save(input_file + '.model')
+model.save_word2vec_format(input_file + '.vec')
+
 sent_file = 'arxiv_words.txt'
-model = Sent2Vec(LineSentence(sent_file), model_file='arxiv_words.model')
+model = Sent2Vec(LineSentence(sent_file), model_file=input_file + '.model')
 model.save_sent2vec_format(sent_file + '.vec')
 
 program = os.path.basename(sys.argv[0])
