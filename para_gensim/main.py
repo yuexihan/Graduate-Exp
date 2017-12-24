b65e806592ed8a72bc2a9ec6549dc6b72c61d8cb
leon <463691529@qq.com>
2017-12-24 17:53:08 +0800

    fix bug

--- para_gensim/main.py
+++ para_gensim/main.py
@@ -1,4 +1,5 @@
 import gensim
 
-model = gensim.models.Doc2Vec('data/arxiv_words', size=100, window=8, min_count=5, workers=5)
+docs = gensim.models.doc2vec.TaggedLineDocument('data/arxiv_words')
+model = gensim.models.Doc2Vec(docs, size=100, window=8, min_count=5, workers=5)
 model.save('save/doc2vec.model')
