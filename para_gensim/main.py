7e813b827ae6f3df0d709c63b19d9ea3a33d0e83
leon <463691529@qq.com>
2017-12-24 17:44:34 +0800

    fix bug

--- para_gensim/main.py
+++ para_gensim/main.py
@@ -1,4 +1,4 @@
-from gensim.model import Doc2Vec
+import gensim
 
-model = Doc2Vec('data/arxiv_words', size=100, window=8, min_count=5, workers=5)
+model = gensim.model.Doc2Vec('data/arxiv_words', size=100, window=8, min_count=5, workers=5)
 model.save('save/doc2vec.model')
