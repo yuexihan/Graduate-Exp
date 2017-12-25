82ecb8e460129f5e1fe0e86cb52fd215b620c4e9
leon <463691529@qq.com>
2017-12-26 00:04:08 +0800

    fix bug

--- ieee-deepwalk/sentence2vec/main.py
+++ ieee-deepwalk/sentence2vec/main.py
@@ -17,7 +17,7 @@ from word2vec import Word2Vec, Sent2Vec, LineSentence
 logging.basicConfig(format='%(asctime)s : %(threadName)s : %(levelname)s : %(message)s', level=logging.INFO)
 logging.info("running %s" % " ".join(sys.argv))
 
-input_file = 'ieee-deepwalk.txt'
+input_file = 'ieee-deepwalk-2014.txt'
 model = Word2Vec(LineSentence(input_file), size=100, window=5, sg=0, min_count=5, workers=8)
 model.save(input_file + '.model')
 model.save_word2vec_format(input_file + '.vec')
