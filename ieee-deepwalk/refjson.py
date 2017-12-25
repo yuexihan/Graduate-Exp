c8112b0e9f6103b3a682f00e8dfe1c1f281de690
leon <463691529@qq.com>
2017-12-25 18:38:49 +0800

    fix bug

--- ieee-deepwalk/refjson.py
+++ ieee-deepwalk/refjson.py
@@ -40,10 +40,10 @@ for line in open('edges.txt'):
 
 j = []
 for a in nodes:
-    l = {'id': a}
+    l = {'id': a, 'allcats': ''}
     j.append(l)
 for a,b in links:
-    l = {'id': a, 'ref': [(b, 1)]}
+    l = {'id': a, 'ref': [(b, 1)], 'allcats': ''}
     j.append(l)
 
 with open('edges.json', 'w') as f:
