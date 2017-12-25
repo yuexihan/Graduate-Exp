045879a7a47b696619d0236a29f97711b2dc738c
leon <463691529@qq.com>
2017-12-25 18:31:18 +0800

    fix bug

---
+++ ieee-deepwalk/refjson.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+import json
+
+g = open('ieee_category.txt')
+
+category = []
+for line in g:
+        category.append(line.strip())
+
+print len(category)
+
+nodes = set()
+d = defaultdict(set)
+for line in 'edges.txt':
+    a, b = line.split()
+    a = int(a)
+    b = int(b)
+    d[a].add(b)
+    nodes.add(a)
+    nodes.add(b)
+
+j = []
+for a in nodes:
+    l = {}
+    l['id'] = a
+    l['allcats'] = category[a]
+    l['refs'] = []
+    if a in d:
+        for b in d[a]:
+            l['refs'].append([b, 1])
+    j.append(l)
+
+with open('edges.json', 'w') as f:
+    json.dump(j, f)
