2862e6eaacf8a8dec5883e05a9bebf837601bfdd
leon <463691529@qq.com>
2017-12-25 18:37:15 +0800

    fix bug

--- ieee-deepwalk/refjson.py
+++ ieee-deepwalk/refjson.py
@@ -7,25 +7,43 @@ for line in open('ieee_category.txt'):
 
 print len(category)
 
+# nodes = set()
+# d = defaultdict(set)
+# for line in open('edges.txt'):
+#     a, b = line.split()
+#     a = int(a)
+#     b = int(b)
+#     d[a].add(b)
+#     nodes.add(a)
+#     nodes.add(b)
+#
+# j = []
+# for a in nodes:
+#     l = {}
+#     l['id'] = a
+#     l['allcats'] = category[a]
+#     l['refs'] = []
+#     if a in d:
+#         for b in d[a]:
+#             l['refs'].append([b, 1])
+#     j.append(l)
+
 nodes = set()
-d = defaultdict(set)
+links = set()
 for line in open('edges.txt'):
     a, b = line.split()
     a = int(a)
     b = int(b)
-    d[a].add(b)
     nodes.add(a)
     nodes.add(b)
+    links.add((a,b))
 
 j = []
 for a in nodes:
-    l = {}
-    l['id'] = a
-    l['allcats'] = category[a]
-    l['refs'] = []
-    if a in d:
-        for b in d[a]:
-            l['refs'].append([b, 1])
+    l = {'id': a}
+    j.append(l)
+for a,b in nodes:
+    l = {'id': a, 'ref': [(b, 1)]}
     j.append(l)
 
 with open('edges.json', 'w') as f:
