639e4530290b3121fcf0104a05cfc1a6905d7b0e
leon <463691529@qq.com>
2017-12-26 10:03:11 +0800

    fix bug

--- sentence2vec/main.py
+++ sentence2vec/main.py
@@ -19,7 +19,7 @@ logging.info("running %s" % " ".join(sys.argv))
 input_file = 'arxiv_words.txt'
 model = Word2Vec(LineSentence(input_file), size=100, window=5, sg=0, min_count=5, workers=8)
 model.save(input_file + '.model')
-model.save_word2vec_format(input_file + '.vec')
+model.save_word2vec_format('words' + '.vec')
 
 sent_file = 'arxiv_words.txt'
 model = Sent2Vec(LineSentence(sent_file), model_file=input_file + '.model')
