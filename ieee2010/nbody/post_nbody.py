e5a22abf5c3946ee50141c2255c2a8d6ce7fab37
leon <463691529@qq.com>
2017-12-28 01:57:00 +0800

    fix bug

--- ieee2010/nbody/post_nbody.py
+++ ieee2010/nbody/post_nbody.py
@@ -3,11 +3,17 @@ import csv
 
 l = json.load(open('map-387667.json'))
 
+categories = []
+for line in open('ieee_category.txt'):
+    categories.append(line)
+
 d = {}
-with open('ieee_vector_nbody.txt','w') as f:
-    for i, (j, x, y, r) in enumerate(l):
-        f.write(str(x) + ' ' + str(y) + '\n')
-        d[j] = i
+with open('ieee_label_nbody.txt', 'w') as f1:
+    with open('ieee_vector_nbody.txt','w') as f2:
+        for i, (j, x, y, r) in enumerate(l):
+            f2.write(categories[j])
+            f2.write(str(x) + ' ' + str(y) + '\n')
+            d[j] = i
 
 with open('ieee_reference.csv', 'rb') as fin:
     with open('ieee_reference_nbody.csv', 'wb') as fout:
@@ -17,6 +23,7 @@ with open('ieee_reference.csv', 'rb') as fin:
         for i, j in reader:
             i = int(i)
             j = int(j)
-            i = d[i]
-            j = d[j]
-            writer.writerow([i, j])
+            if i in d and j in d:
+                i = d[i]
+                j = d[j]
+                writer.writerow([i, j])
