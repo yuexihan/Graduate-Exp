eafb7108954c561bebe1a1a89b7d40e66e25d1af
leon <463691529@qq.com>
2017-12-29 12:20:22 +0800

    lr

---
+++ ieee2010/sentence2vec/tf_vec_visualize.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import numpy as np
+import os
+from tensorflow.contrib.tensorboard.plugins import projector
+
+paper_embedding = []
+with open('ieeeVectorAvg.txt') as f:
+    for line in f:
+        line = [float(x) for x in line.split()]
+        paper_embedding.append(line)
+
+paper_embedding = tf.Variable(paper_embedding, trainable=False, name='paper_embedding', dtype=tf.float32)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+tf.train.Saver().save(sess, 'save/best')
+
+config = projector.ProjectorConfig()
+
+paper_projector = config.embeddings.add()
+paper_projector.tensor_name = paper_embedding.name
+paper_projector.metadata_path = 'ieee_category.txt'
+
+if not os.path.exists('save'):
+    os.makedirs('save')
+summary_writer = tf.summary.FileWriter('save')
+projector.visualize_embeddings(summary_writer, config)
+
